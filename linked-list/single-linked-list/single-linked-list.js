adbb777ec7850f1abb19a6e653461afec6d72cdd
Grisha Yepiskoposyan <grisha.yepiskoposyan.dev@gmail.com>
2024-06-12 19:59:36 +0400

    Added single linked list

---
+++ linked-list/single-linked-list/single-linked-list.js
@@ -0,0 +1,38 @@
+class ListNode {
+    constructor(val) {
+        this.val = val;
+    }
+}
+
+class SingleLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insert(val) {
+        if(!this.head){
+            this.head = new ListNode(val);
+            this.tail = this.head;
+            return;
+        }
+        this.tail.next = new ListNode(val);
+        this.tail = this.tail.next
+    }
+
+    print(){
+        let curr = this.head
+        let res = ''
+        while(curr){
+            res += curr.val + ' -> ';
+            curr = curr.next;
+        }
+        return res
+    }
+}
+
+const singleLinkedList = new SingleLinkedList()
+singleLinkedList.insert(4)
+singleLinkedList.insert(5)
+singleLinkedList.insert(2)
+console.log(singleLinkedList.print())
