c3c2aee86c28b0a3a33426e9effd43fd1b08a3e2
Grisha Yepiskoposyan <grisha.yepiskoposyan.dev@gmail.com>
2024-06-13 22:11:38 +0400

    Fixed Double Linked List

--- linked-list/double-liked-list/double-linked-list.js
+++ linked-list/double-liked-list/double-linked-list.js
@@ -2,6 +2,7 @@ class ListNode {
     constructor(val) {
         this.val = val;
         this.next = null;
+        this.prev = null;
     }
 }
 
@@ -9,18 +10,90 @@ class DoubleLinkedList{
     constructor() {
         this.head = null;
         this.tail = null;
+        this.size = 0;
     }
 
-    insert(val){
-        let temp = new ListNode(val)
+    insertEnd(val){
+        const newNode = new ListNode(val)
         if(!this.head) {
-            this.head = temp;
-            this.tail = temp;
+            this.head = newNode;
+            this.tail = newNode;
+            this.size++;
             return;
         }
-        temp.prev = this.tail;
-        this.tail.next = temp;
-        this.tail = this.tail.next;
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode
+        this.size++
+    }
+
+    insertFront(val){
+        const newNode = new ListNode(val)
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+            this.size++;
+            return;
+        }
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode
+        this.size++
+    }
+
+    insertAtIndex(index, val){
+        if(index < 0 || this.size <= index){
+            return;
+        }
+        if(!this.head){
+            this.insertFront(val)
+        } else if(index === this.size - 1){
+            console.log(index, this.size - 1)
+            this.insertEnd(val)
+        } else {
+            let curr = this.head;
+            let i = 0;
+            while(i !== index){
+                curr = curr.next;
+                i++;
+            }
+
+            const newNode = new ListNode(val);
+            newNode.next = curr;
+            newNode.prev = curr.prev;
+            curr.prev.next = newNode;
+            curr.prev = newNode;
+            this.size++;
+        }
+    }
+
+    deleteAtIndex(index){
+        if(index < 0 || this.size <= index){
+            return;
+        }
+        if(index === 0){
+            if(this.size === 1){
+                this.head = null;
+                this.tail = null
+            } else {
+                this.head = this.head.next;
+                this.head.prev = null
+            }
+        } else if(index === this.size -1){
+            this.tail = this.tail.prev;
+            this.tail.next = null;
+        } else {
+            let curr = this.head;
+            let i = 0;
+            while(i !== index){
+                curr = curr.next;
+                i++;
+            }
+
+            curr.next.prev = curr.prev;
+            curr.prev.next = curr.next;
+        }
+        this.size--;
     }
 
     print(){
@@ -35,7 +108,10 @@ class DoubleLinkedList{
 }
 
 const doubleLinkedList = new DoubleLinkedList()
-doubleLinkedList.insert(2)
-doubleLinkedList.insert(6)
-doubleLinkedList.insert(5)
+doubleLinkedList.insertEnd(2)
+doubleLinkedList.insertEnd(6)
+doubleLinkedList.insertFront(7)
+doubleLinkedList.insertEnd(5)
+doubleLinkedList.insertAtIndex(2, 8)
+doubleLinkedList.deleteAtIndex(1)
 console.log(doubleLinkedList.print())
